cd42033b5e9bdec212c2f1080ec9b73840c9266f
Frank Wang <fraankwang@gmail.com>
2016-04-07 16:52:50 -0400

    added GameEditorGrid for consistency, changed TabPane for LevelsGrid to be in LevelsDisplay instead

--- src/authoring_environment/frontend/display_elements/tab_displays/LevelsTabDisplay.java
+++ src/authoring_environment/frontend/display_elements/tab_displays/LevelsTabDisplay.java
@@ -1,16 +1,24 @@
 package authoring_environment.frontend.display_elements.tab_displays;
 
-import authoring_environment.frontend.display_elements.grids.tab_grids.LevelsTabGrid;
 import authoring_environment.frontend.interfaces.display_element_interfaces.editor_display_interfaces.IEditorDisplay;
 import authoring_environment.frontend.interfaces.display_element_interfaces.tab_display_interfaces.ITabDisplays.ILevelsTabDisplay;
 import javafx.scene.Node;
+import javafx.scene.control.TabPane;
+
+/**
+ * The LevelsTabDisplay contains multiple tabs of a set of user-created levels depending on
+ * which Mode is selected (sets of levels correspond to each Mode created).
+ * 
+ * @author Frank, benchesnut
+ *
+ */
 
 public class LevelsTabDisplay implements ILevelsTabDisplay {
 
-	private LevelsTabGrid myLevelsTabGrid;
+	private TabPane myLevelsTabPane;
 
 	public LevelsTabDisplay() {
-		myLevelsTabGrid = new LevelsTabGrid();
+		myLevelsTabPane = new TabPane();
 
 	}
 
