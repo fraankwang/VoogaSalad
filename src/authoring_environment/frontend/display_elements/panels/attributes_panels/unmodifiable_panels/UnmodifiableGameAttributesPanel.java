8f1f5c10e15276b21ae39ef59f6a161cd0eaebbd
Frank Wang <fraankwang@gmail.com>
2016-04-10 11:27:02 -0400

    added new panel package and IPanel interface

---
+++ src/authoring_environment/frontend/display_elements/panels/attributes_panels/unmodifiable_panels/UnmodifiableGameAttributesPanel.java
@@ -0,0 +1,30 @@
+package authoring_environment.frontend.display_elements.panels.attributes_panels.unmodifiable_panels;
+
+import authoring_environment.frontend.display_elements.panels.attributes_panels.AttributesPanel;
+
+/**
+ * 
+ * @author Frank
+ *
+ */
+
+public class UnmodifiableGameAttributesPanel extends AttributesPanel {
+
+	public UnmodifiableGameAttributesPanel(int height, int width) {
+		super(height, width);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void initializeComponents() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void assembleComponents() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/authoring_environment/frontend/display_elements/panels/button_dashboards/StandardButtonDashboard.java
@@ -0,0 +1,35 @@
+package authoring_environment.frontend.display_elements.panels.button_dashboards;
+
+import javafx.scene.control.Button;
+
+/**
+ * The StandardButtonDashboard contains a duplicate and delete option in
+ * addition to the save and reset changes option.
+ * 
+ * @author Frank
+ *
+ */
+
+public class StandardButtonDashboard extends SimpleButtonDashboard {
+
+	private Button myDuplicateButton;
+	private Button myDeleteButton;
+
+	public StandardButtonDashboard(int height, int width) {
+		super(height, width);
+
+	}
+
+	@Override
+	protected void initializeComponents() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void assembleComponents() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
