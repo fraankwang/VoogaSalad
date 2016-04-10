9095283998f18121fbd4c9b2421290b6915885bd
Christine Zhou <zhou.l.christine@gmail.com>
2016-04-10 18:23:40 -0400

    Making tmodifications to xml testing class.

--- src/backend/xml_converting/XMLTesting.java
+++ src/backend/xml_converting/XMLTesting.java
@@ -18,11 +18,11 @@ public class XMLTesting {
 		xmlWriter.writeStartTag("levels");
 		xmlWriter.writeStartTag("backend.Level");
 		xmlWriter.writeStartTag("entities");
-		xmlWriter.writeStartTag("backend.Entity");
+		xmlWriter.writeStartTag("backend.game__object.entities.Entity");
 		xmlWriter.writeStartTag("myID");
 		xmlWriter.writeCharacters("0");
 		xmlWriter.writeEndTag();
-		xmlWriter.writeStartTag("components");
+		xmlWriter.writeStartTag("myComponents");
 		xmlWriter.writeStartTag("backend.DisplayComponent");
 		xmlWriter.writeEndTag();
 		xmlWriter.writeEndTag();
@@ -36,11 +36,11 @@ public class XMLTesting {
 		xmlWriter.writeStartTag("currentMode");
 		xmlWriter.writeCharacters("0");
 		xmlWriter.writeEndTag();
-		xmlWriter.writeStartTag("currentLevel");
-		xmlWriter.writeCharacters("0");
-		xmlWriter.writeEndTag();
 		xmlWriter.writeStartTag("numOfLevels");
 		xmlWriter.writeCharacters("1");
+		xmlWriter.writeEndTag();
+		xmlWriter.writeStartTag("currentLevel");
+		xmlWriter.writeCharacters("0");
 		xmlWriter.writeEndTag();
 		xmlWriter.writeStartTag("numOfModes");
 		xmlWriter.writeCharacters("1");
