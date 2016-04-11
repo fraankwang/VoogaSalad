62476f4eab9434cea298251d257195c44f269ff1
Austin Wu <austintwu@gmail.com>
2016-04-10 23:25:22 -0400

    uncommented starting dialog

--- src/main/Main.java
+++ src/main/Main.java
@@ -23,26 +23,26 @@ public class Main extends Application {
      */
     @Override
     public void start (Stage stage) {
-////    	Alert alert = new Alert(AlertType.CONFIRMATION);
-////    	alert.setTitle("Select Program");
-////    	alert.setHeaderText("Select Program");
-////    	alert.setContentText("Choose your program type");
-////
-////    	ButtonType buttonTypeAuthor = new ButtonType("Author");
-////    	ButtonType buttonTypePlayer = new ButtonType("Player");
-////    	ButtonType buttonTypeCancel = new ButtonType("Cancel", ButtonData.CANCEL_CLOSE);
-////    	
-////    	alert.getButtonTypes().setAll(buttonTypeAuthor, buttonTypePlayer, buttonTypeCancel);
-////    	Optional<ButtonType> result = alert.showAndWait();
-////    	
-//    	if (result.get() == buttonTypeAuthor){
-//    	    // GAME AUTHOR FRONTEND SCENE CREATED HERE
-//    	} else if (result.get() == buttonTypePlayer) {
+    	Alert alert = new Alert(AlertType.CONFIRMATION);
+    	alert.setTitle("Select Program");
+    	alert.setHeaderText("Select Program");
+    	alert.setContentText("Choose your program type");
+
+    	ButtonType buttonTypeAuthor = new ButtonType("Author");
+    	ButtonType buttonTypePlayer = new ButtonType("Player");
+    	ButtonType buttonTypeCancel = new ButtonType("Cancel", ButtonData.CANCEL_CLOSE);
+    	
+    	alert.getButtonTypes().setAll(buttonTypeAuthor, buttonTypePlayer, buttonTypeCancel);
+    	Optional<ButtonType> result = alert.showAndWait();
+    	
+    	if (result.get() == buttonTypeAuthor){
+    	    // GAME AUTHOR FRONTEND SCENE CREATED HERE
+    	} else if (result.get() == buttonTypePlayer) {
     		EngineView engineView = new EngineView(); 
     		Scene scene = engineView.getScene();
             stage.setScene(scene);
             stage.show();
-//    	}
+    	}
     }
 
     private Scene getStartDialogue(){
