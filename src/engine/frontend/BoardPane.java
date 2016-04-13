f4aea8d9520acec25f25c0aee1b8ab84f98b2bcf
Hayden Bader <hpb3@duke.edu>
2016-04-12 20:52:51 -0400

    commiting base board pane changes preparing to pull

--- src/engine/frontend/BoardPane.java
+++ src/engine/frontend/BoardPane.java
@@ -1,15 +1,23 @@
 package engine.frontend;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import engine.controller.EngineController;
 import javafx.scene.Node;
 import javafx.scene.layout.GridPane;
 import javafx.scene.shape.Rectangle;
 
 public class BoardPane {
 	private EngineView myEngineView;
+	private EngineController myController;
+	private Map<String, EntityView> myBoardEntities = new HashMap<>();
 	
-	public BoardPane(EngineView ev){
+	public BoardPane(EngineView ev, EngineController ec){
 		myEngineView = ev;
+		myController = ec;
 	}
+	
 	
 	public Node buildNode(){
 		
@@ -19,6 +27,27 @@ public class BoardPane {
 		gridpane.setMinHeight(myEngineView.loadUIIntResource("BoardSize"));
 		gridpane.setMaxHeight(myEngineView.loadUIIntResource("BoardSize"));
 		
+		gridpane.setOnMouseClicked(e -> attemptTowerDrop(e.getSceneX(), e.getSceneY()));
+		//gridpane.setOnDragDropped(e -> attemptTowerDrop(e.getSceneX(), e.getSceneY()));
+		
 		return gridpane;
 	}
+	
+	public void attemptTowerDrop(double x, double y){
+		myController.attemptTowerDrop(x, y);
+	}
+	
+	public void step(){
+		Map<String, EntityView> controllerBoardEntities = myController.getMap();
+		for(String key: controllerBoardEntities.keySet()){
+			if( controllerBoardEntities.get(key).hasChanged()){
+				if(myBoardEntities.containsValue(key)){
+					
+				}else{
+					myBoardEntities.put(key, controllerBoardEntities.get(key));
+				}
+			}
+		}
+	}
+	
 }
