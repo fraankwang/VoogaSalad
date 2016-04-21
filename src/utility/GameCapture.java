a04ec17616b2a08a38f5e140b23bd36bc2565fa3
Austin Wu <austintwu@gmail.com>
2016-04-21 15:35:41 -0400

    finished the non threading version of game capture, now trying thread version

--- src/utility/GameCapture.java
+++ src/utility/GameCapture.java
@@ -1,11 +1,6 @@
 package utility;
 
-import java.awt.AWTException;
-import java.awt.Dimension;
 import java.awt.Event;
-import java.awt.Rectangle;
-import java.awt.Robot;
-import java.awt.Toolkit;
 import java.awt.image.BufferedImage;
 import java.util.ResourceBundle;
 import java.util.concurrent.TimeUnit;
@@ -13,9 +8,12 @@ import java.util.concurrent.TimeUnit;
 import com.xuggle.mediatool.IMediaWriter;
 import com.xuggle.mediatool.ToolFactory;
 import com.xuggle.xuggler.ICodec;
-import com.xuggle.xuggler.IRational;
 
 import engine.frontend.overall.EngineView;
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.util.Duration;
 
 public class GameCapture implements IGameCapture {
 
@@ -31,6 +29,7 @@ public class GameCapture implements IGameCapture {
 	private IMediaWriter fileWriter;
 
 	private boolean capture;
+	private Timeline myTimeline;
 
 	public GameCapture(EngineView ev) {
 		myResources = ResourceBundle.getBundle(DEFAULT_RESOURCE);
@@ -39,52 +38,49 @@ public class GameCapture implements IGameCapture {
 		fileName = myResources.getString("DefaultName");
 		imageFormat = ICodec.ID.CODEC_ID_PNG;
 		videoFormat = ICodec.ID.CODEC_ID_MPEG4;
-		fps = 10;
+		fps = 5;
 	}
 
 	@Override
 	public void startCapture() {
 		fileWriter = ToolFactory.makeWriter(fileName + System.currentTimeMillis() + ".mp4");
-		fileWriter.addVideoStream(0, 0, videoFormat, IRational.make(1 / fps),
-				(int) myEngineView.getStage().getWidth() / 2, (int) myEngineView.getStage().getHeight() / 2);
-		capture = true;
+		fileWriter.addVideoStream(0, 0, videoFormat, 
+				(int) myEngineView.getStage().getWidth(), (int) myEngineView.getStage().getHeight());
 		record();
 
 	}
 
 	private void record() {
 		long startTime = System.nanoTime();
-		while (capture) {
-			System.out.println("Taking a frame");
-			// take the screen shot
-			BufferedImage screen = myEngineView.getStageShot();
-//			BufferedImage screen = getDesktopScreenshot();
+		
+		myTimeline = new Timeline(new KeyFrame(
+		        Duration.millis(1000/fps),
+		        ae -> takeAFrame(startTime)));
+		myTimeline.setCycleCount(Animation.INDEFINITE);
+		myTimeline.play();
+	}
+	
+	private void takeAFrame(long startTime){
+		System.out.println("Taking a frame");
+		// take the screen shot
+		BufferedImage screen = myEngineView.getStageShot();
 
-			// convert to the right image type
-			BufferedImage bgrScreen = convertToType(screen, BufferedImage.TYPE_3BYTE_BGR);
+		// convert to the right image type
+		BufferedImage bgrScreen = convertToType(screen, BufferedImage.TYPE_3BYTE_BGR);
 
-			// encode the image to stream #0
-			fileWriter.encodeVideo(0, bgrScreen, System.nanoTime() - startTime, TimeUnit.NANOSECONDS);
-
-			// sleep for frame rate milliseconds
-			try {
-				Thread.sleep((long) (1000 / fps));
-			} catch (InterruptedException e) {
-				// ignore
-			}
-
-		}
+		// encode the image to stream #0
+		fileWriter.encodeVideo(0, bgrScreen, System.nanoTime() - startTime, TimeUnit.NANOSECONDS);
+		capture = true;
 	}
 
 	@Override
 	public void pauseCapture(Event pauseCaptureEvent) {
 		// TODO need to add way to re-begin screen capture
-		setCapture(false);
 	}
 
 	@Override
 	public void endCapture() {
-		setCapture(false);
+		myTimeline.stop();
 		fileWriter.close();
 	}
 
@@ -124,10 +120,6 @@ public class GameCapture implements IGameCapture {
 		fileName = f;
 	}
 
-	private void setCapture(boolean updated) {
-		capture = updated;
-	}
-
 	private BufferedImage convertToType(BufferedImage sourceImage, int targetType) {
 
 		BufferedImage image;
@@ -145,18 +137,5 @@ public class GameCapture implements IGameCapture {
 		}
 
 		return image;
-	}
-
-	private static BufferedImage getDesktopScreenshot() {
-		Dimension screenBounds = Toolkit.getDefaultToolkit().getScreenSize();
-		try {
-			Robot robot = new Robot();
-			Rectangle captureSize = new Rectangle(screenBounds);
-			return robot.createScreenCapture(captureSize);
-		} catch (AWTException e) {
-			e.printStackTrace();
-			return null;
-		}
-
 	}
 }
