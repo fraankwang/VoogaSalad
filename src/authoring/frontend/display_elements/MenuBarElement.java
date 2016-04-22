cf7d9cf07dde949cd376418ad6e705c71b355aa4
Frank Wang <fraankwang@gmail.com>
2016-04-22 11:13:16 -0400

    added help menu options

--- src/authoring/frontend/display_elements/MenuBarElement.java
+++ src/authoring/frontend/display_elements/MenuBarElement.java
@@ -2,10 +2,14 @@ package authoring.frontend.display_elements;
 
 import authoring.frontend.interfaces.display_element_interfaces.IMenuBarElement;
 import authoring.frontend.interfaces.display_element_interfaces.ITabBarElement;
+import javafx.scene.Group;
 import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
+import javafx.scene.web.WebView;
+import javafx.stage.Stage;
 
 /**
  * The MenuBarElement acts as a primary UI component that creates and manages
@@ -35,7 +39,7 @@ public class MenuBarElement implements IMenuBarElement {
 		myMenuBar = new MenuBar();
 		Menu file = createFileMenu();
 		Menu create = new Menu("Create");
-		Menu help = new Menu("Help");
+		Menu help = createHelpMenu();
 
 		myMenuBar.getMenus().addAll(file, create, help);
 	}
@@ -56,6 +60,35 @@ public class MenuBarElement implements IMenuBarElement {
 		file.getItems().add(open);
 		open.getItems().addAll(openGame, openModes, openLevels, openEntities);
 		return file;
+	}
+
+	private Menu createHelpMenu() {
+		Menu help = new Menu("Help");
+
+		MenuItem openWiki = new MenuItem("About");
+		openWiki.setOnAction(
+				e -> openWebPage("https://github.com/duke-compsci308-spring2016/voogasalad_DrumpfTower/wiki"));
+		MenuItem openTDWiki = new MenuItem("How to play Tower Defense");
+		openTDWiki.setOnAction(e -> openWebPage("https://en.wikipedia.org/wiki/Tower_defense"));
+
+		help.getItems().addAll(openWiki, openTDWiki);
+		return help;
+	}
+
+	private void openWebPage(String url) {
+		Stage helpStage = new Stage();
+
+		WebView webView = new WebView();
+		webView.getEngine().load(url);
+		webView.setPrefSize(800, 800);
+
+		Group root = new Group();
+		root.getChildren().add(webView);
+
+		Scene scene = new Scene(root, 800, 800);
+		helpStage.setScene(scene);
+		helpStage.show();
+
 	}
 
 	@Override
