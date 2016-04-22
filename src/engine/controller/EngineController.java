c6b2b3775106ce2bc610179f8170c381da2fc9db
Raghav <raghav.kedia@duke.edu>
2016-04-22 17:05:42 -0400

    added testing class

--- src/engine/controller/EngineController.java
+++ src/engine/controller/EngineController.java
@@ -35,6 +35,9 @@ public class EngineController implements IEngineController{
 	private EventManager myEventManager;
 	private GameWorld myGameWorld;
 	private SystemsController mySystems;
+	
+	//testing
+	private testingClass myTestingClass;
 
 	private EngineView myEngineView;
 
@@ -44,11 +47,14 @@ public class EngineController implements IEngineController{
 		myEngineView = new EngineView(myStage, this); 
         myStage.setScene(myEngineView.buildScene());
         myStage.show();
+        
 	}
 	
 	public void start(){
 		myGameWorld = new GameWorld();
-		initTestGame();
+		myTestingClass = new testingClass(myGameWorld);
+		myGameWorld = myTestingClass.initTestGame();
+		//initTestGame();
 		
 		myEventManager = new EventManager(this, myGameWorld);
 		mySystems = new SystemsController(NUM_FRAMES_PER_SECOND, myEventManager);
@@ -110,63 +116,5 @@ public class EngineController implements IEngineController{
 	public GameWorld getMyGameWorld(){
 		return myGameWorld;
 	}
-	
-	private void initTestGame(){
-		myGameWorld = new GameWorld();
-		Mode tempMode = new Mode("tempMode");
-		Level tempLevel = new Level(0);
-		Path tempPath = new Path();
-		BezierCurve tempCurve1 = new BezierCurve(0,0, 0,0, 0,0, 200,200);
-		BezierCurve tempCurve2 = new BezierCurve(200,200, 50,50, 150,150, 0,300);
-		BezierCurve tempCurve3 = new BezierCurve(0,300, 150, 150, 250, 250, 400,400);
-		
-		tempPath.addCurve(tempCurve1);
-		tempPath.addCurve(tempCurve2);
-		tempPath.addCurve(tempCurve3);
-		
-		GameMap tempMap = new GameMap("", tempPath, 200, 200);
-		
-		IEntity tempEntity = new Entity(0, "tempEntity", "object", 20);
-		IComponent tempPosition = new PositionComponent(0, 60);
-		IComponent tempMovement = new MovementComponent(2, 0);
-		IComponent tempCollision = new CollisionComponent();
-		//IComponent pathComp = new PathComponent(0, 0);
-		IComponent tempDisplay = new DisplayComponent("DrumpfVader.png");
-		IComponent tempSize = new SizeComponent();
-		tempEntity.addComponent(tempDisplay);
-		tempEntity.addComponent(tempSize);
-		tempEntity.addComponent(tempPosition);
-		tempEntity.addComponent(tempMovement);
-		tempEntity.addComponent(tempCollision);
-		//tempEntity.addComponent(pathComp);
-		
-		IEntity tempEntity2 = new Entity(1, "tempEntity2", "object2", 20);
-		IComponent tempPosition2 = new PositionComponent(700, 60);
-		IComponent tempMovement2 = new MovementComponent(-4, 0);
-		//IComponent pathComp2 = new PathComponent(0, 0);
-		IComponent tempDisplay2 = new DisplayComponent("DrumpfVader.png");
-		IComponent tempSize2 = new SizeComponent();
-		IComponent tempCollision2 = new CollisionComponent();
-		tempEntity2.addComponent(tempDisplay2);
-		tempEntity2.addComponent(tempSize2);
-		tempEntity2.addComponent(tempPosition2);
-		tempEntity2.addComponent(tempMovement2);
-		tempEntity.addComponent(tempCollision2);
-		//tempEntity2.addComponent(pathComp2);
-		
-		IEntity tempEntity3 = new Entity(2, "tempEntity3", "object3", 20);
-		IComponent tempPosition3 = new PositionComponent(450, 450);
-		IComponent tempDisplay3 = new DisplayComponent("DrumpfVader.png");
-		IComponent tempSize3 = new SizeComponent();
-		tempEntity3.addComponent(tempDisplay3);
-		tempEntity3.addComponent(tempSize3);
-		tempEntity3.addComponent(tempPosition3);
-		
-		tempLevel.addToEntities(tempEntity);
-		tempLevel.addToEntities(tempEntity2);
-		tempLevel.addToEntities(tempEntity3);
-		tempLevel.setMap(tempMap);
-		tempMode.addLevel(tempLevel);
-		myGameWorld.addMode(tempMode);
-	}
+
 }
