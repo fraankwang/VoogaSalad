8c2f75077d092a2a86ce1fe923ed307dbecd9851
Austin Wu <austintwu@gmail.com>
2016-04-25 15:05:24 -0400

    added control buttons

--- src/engine/frontend/status/StatusPane.java
+++ src/engine/frontend/status/StatusPane.java
@@ -1,18 +1,33 @@
 package engine.frontend.status;
 
+import java.util.ResourceBundle;
+
 import engine.frontend.overall.EngineView;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
+
+/*
+ * Todos:
+ * Add button enabling based on current game status
+ */
 
 public class StatusPane {
 	private EngineView myEngineView;
 	private Pane myPane;
 	
+	public static final String DEFAULT_RESOURCE = "engine/frontend/status/statuspane";
+	private ResourceBundle myResources;
+	
+	private ControlManager myControlManager;
+	
 	public StatusPane(EngineView ev){
 		myEngineView = ev;
+		myResources = ResourceBundle.getBundle(DEFAULT_RESOURCE);
+		myControlManager = new ControlManager(this);
 	}
 	
 	public Node buildNode(){
@@ -22,30 +37,28 @@ public class StatusPane {
 		myPane.minHeightProperty().bind(myEngineView.getUsableHeight(myEngineView.loadDoubleResource("StatusHeight")));
 		myPane.maxWidthProperty().bind(myEngineView.getUsableWidth(myEngineView.loadDoubleResource("StatusWidth")));
 		myPane.maxHeightProperty().bind(myEngineView.getUsableHeight(myEngineView.loadDoubleResource("StatusHeight")));
-		myPane.getChildren().add(buildRecordControl());
+		
+		HBox hbox = new HBox();
+		hbox.getChildren().add(buildRecordControls());
+		hbox.getChildren().add(myControlManager.buildGameControls());
+		
+		myPane.getChildren().add(hbox);
 		return myPane;
 	}
 	
-	private VBox buildRecordControl(){
-		VBox myVBox = new VBox();
+	private VBox buildRecordControls(){
+		VBox vbox = new VBox();
 		
-		Button record = new Button("Record");
-		Button stop = new Button("Stop");
-		Button picture = new Button("Picture");
+		Button record = createButton(myResources.getString("RecordLabel"));
+		Button stop = createButton(myResources.getString("StopRecordLabel"));
+		Button picture = createButton(myResources.getString("PictureLabel"));
 		
-		record.setMaxHeight(Double.MAX_VALUE);
-		record.setMaxWidth(Double.MAX_VALUE);
-		VBox.setVgrow(record, Priority.ALWAYS);
-		record.setDisable(false);
 		record.setOnAction(e -> {
 			myEngineView.getGameCapture().startCapture();
 			record.setDisable(true);
 			stop.setDisable(false);
 		});
 		
-		stop.setMaxHeight(Double.MAX_VALUE);
-		stop.setMaxWidth(Double.MAX_VALUE);
-		VBox.setVgrow(stop, Priority.ALWAYS);
 		stop.setDisable(true);
 		stop.setOnAction(e -> {
 			myEngineView.getGameCapture().endCapture();
@@ -53,20 +66,30 @@ public class StatusPane {
 			stop.setDisable(true);
 		});
 		
-		picture.setMaxHeight(Double.MAX_VALUE);
-		picture.setMaxWidth(Double.MAX_VALUE);
-		VBox.setVgrow(picture, Priority.ALWAYS);
 		picture.setOnAction(e -> myEngineView.getGameCapture().takeScreenshot(myEngineView.getBody()));
 		
-		myVBox.minWidthProperty().bind(myPane.widthProperty().divide(4));
-		myVBox.minHeightProperty().bind(myPane.heightProperty());
-		myVBox.maxHeightProperty().bind(myPane.heightProperty());
+		vbox.minWidthProperty().bind(myPane.widthProperty().divide(4));
+		vbox.minHeightProperty().bind(myPane.heightProperty());
+		vbox.maxHeightProperty().bind(myPane.heightProperty());
 		
-		myVBox.getChildren().addAll(record, stop, picture);
-		return myVBox;
+		vbox.getChildren().addAll(record, stop, picture);
+		return vbox;
 	}
+	
+	public Button createButton(String s){
+		Button button = new Button(s);
+		button.setMaxHeight(Double.MAX_VALUE);
+		button.setMaxWidth(Double.MAX_VALUE);
+		VBox.setVgrow(button, Priority.ALWAYS);
+		return button;
+	}
+	
 	
 	private Node buildStatDisplay(String name){
 		return null;
+	}
+	
+	public ResourceBundle getMyResources(){
+		return myResources;
 	}
 }
