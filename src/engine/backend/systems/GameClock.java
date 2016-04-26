709405c7c0ad455722af206fe9a13db732c11bbb
Christine Zhou <zhou.l.christine@gmail.com>
2016-04-26 16:04:27 -0400

    Adding seconds per a loop iteration.

--- src/engine/backend/systems/GameClock.java
+++ src/engine/backend/systems/GameClock.java
@@ -1,31 +1,46 @@
 package engine.backend.systems;
 
 public class GameClock {
-	
+
 	private int fps;
 	private int currentLoopIteration;
 	private double currentSecond;
-	
+
 	public GameClock(int fps) {
 		this.fps = fps;
 	}
-	
+
 	private double convertLoopIterationToSeconds() {
-		return currentLoopIteration/ (double) fps;
+		return currentLoopIteration / (double) fps;
 	}
-	
+
 	public void setCurrentLoopIteration(int currentLoopIteration) {
 		this.currentLoopIteration = currentLoopIteration;
 		currentSecond = convertLoopIterationToSeconds();
 	}
-	
+
+	/**
+	 * Updates the clock each loop iteration.
+	 */
 	public void updateLoopIteration() {
 		currentLoopIteration++;
 		currentSecond = convertLoopIterationToSeconds();
 	}
-	
+
+	/**
+	 * 
+	 * @return The current second.
+	 */
 	public double getCurrentSecond() {
 		return currentSecond;
+	}
+
+	/**
+	 * 
+	 * @return The time in seconds for each loop iteration.
+	 */
+	public double getTimePerLoop() {
+		return 1.0 / (double) fps;
 	}
 
 }
