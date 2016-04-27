6a2cc7eb9fc71e0fa1ee4b0fe585924d33969bc7
Jonathan Ma <jonathan.ma@duke.edu>
2016-04-26 22:38:15 -0400

    fix

--- src/engine/backend/components/Component.java
+++ src/engine/backend/components/Component.java
@@ -9,21 +9,7 @@ package engine.backend.components;
 
 public abstract class Component implements IComponent{
 	
-	private String myParentEntityName;
-	
 	public String getTag(){
 		return this.getClass().getSimpleName();
 	}
-	
-	public void setEntityName(String entityName) {
-		this.myParentEntityName = entityName;
-	}
-	
-	/**
-	 * @return The name of the entity with this component.
-	 */
-	public String getEntityName() {
-		return myParentEntityName;
-	}
-	
 }
