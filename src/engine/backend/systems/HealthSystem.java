8bc7338528205770d6989139dc147f24b54aca21
Christine Zhou <zhou.l.christine@gmail.com>
2016-04-28 14:29:02 -0400

    Fixing bug in health system.

--- src/engine/backend/systems/HealthSystem.java
+++ src/engine/backend/systems/HealthSystem.java
@@ -30,39 +30,20 @@ public class HealthSystem extends GameSystem {
 
 	@Override
 	public void update(Level myLevel, Map<String, Set<Integer>> myEventMap, InGameEntityFactory myEntityFactory, double currentSecond) {
-//		Collection<IEntity> entities = myLevel.getEntities().values();
 		Collection<IEntity> applicableEntities = getEntitiesWithTag(myLevel.getEntities().values(), ComponentTagResources.healthComponentTag);
 		for (IEntity entity : applicableEntities) {
 			HealthComponent healthComp = (HealthComponent) entity.getComponent(ComponentTagResources.healthComponentTag);
+			if(healthComp.getHealth() <= 0){
+				addToEventMap(myEventMap, getDeathEvent(entity), Arrays.asList(entity));
+				continue;
+			}
+			
 			if(healthComp.getHealth() <= healthComp.getCriticalHealth()){
-				//IEvent event = getDeathEvent(entity); 
 				addToEventMap(myEventMap, getCriticalHealthEvent(entity), entity);
 				continue;
 			}
 			
-			if(healthComp.getHealth() <= 0){
-				//IEvent event = getCriticalHealthEvent(entity);
-				addToEventMap(myEventMap, getDeathEvent(entity), Arrays.asList(entity));
-				continue;
-			}
 		}
-//		for(IEntity entity : entities){
-//			if(entity.hasComponent(ComponentTagResources.healthComponentTag)){
-//				HealthComponent healthComp = (HealthComponent) entity.getComponent(ComponentTagResources.healthComponentTag);
-//				
-//				if(healthComp.getHealth() <= healthComp.getCriticalHealth()){
-//					IEvent event = getDeathEvent(entity); 
-//					addToEventMap(myEventMap, event, Arrays.asList(entity));
-//					continue;
-//				}
-//				
-//				if(healthComp.getHealth() <= 0){
-//					IEvent event = getCriticalHealthEvent(entity);
-//					addToEventMap(myEventMap, event, Arrays.asList(entity));
-//					continue;
-//				}
-//			}
-//		}
 	}
 
 	private IEvent getDeathEvent(IEntity entity){
