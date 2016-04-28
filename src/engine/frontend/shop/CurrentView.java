676d09657af2a58ef11c1ddbcf9fbf6dfefd737e
Austin Wu <austintwu@gmail.com>
2016-04-28 14:31:31 -0400

    finished currentview

--- src/engine/frontend/shop/CurrentView.java
+++ src/engine/frontend/shop/CurrentView.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Observable;
 import java.util.Observer;
+import java.util.ResourceBundle;
 
 import engine.backend.entities.IEntity;
 import javafx.beans.binding.DoubleExpression;
@@ -16,9 +17,13 @@ import javafx.scene.control.ListView;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 
 public class CurrentView implements Observer {
 
+	public static final String DEFAULT_RESOURCE = "engine/frontend/shop/statlabels";
+	private ResourceBundle myResources;
+	
 	private ShopPane myShopPane;
 
 	private HBox myHBox;
@@ -30,9 +35,26 @@ public class CurrentView implements Observer {
 
 	private Map<String, Boolean> showMap;
 
+	private boolean debug = true;
+	
 	public CurrentView(ShopPane sp) {
 		myShopPane = sp;
-		showMap = new HashMap<String, Boolean>();
+		showMap = new HashMap<String, Boolean>(); 
+		myResources = ResourceBundle.getBundle(DEFAULT_RESOURCE);
+		if(!debug){
+			addDefaultShows(showMap);
+		}
+	}
+	
+	private void addDefaultShows(Map<String, Boolean> showMap){
+		String[] hides = myResources.getString("DefaultHides").split(",");
+		String[] shows = myResources.getString("DefaultShows").split(",");
+		for(String s: hides){
+			showMap.put(s, false);
+		}
+		for(String s: shows){
+			showMap.put(s, true);
+		}
 	}
 
 	public Node buildCurrentView(DoubleExpression widthBinding, DoubleExpression heightBinding) {
@@ -41,14 +63,16 @@ public class CurrentView implements Observer {
 		myShopPane.bindWidth(myHBox, widthBinding);
 
 		myImageView = new ImageView();
-		myImageView.fitHeightProperty().bind(heightBinding);
+		myImageView.fitHeightProperty().bind(myHBox.heightProperty());
 		myImageView.setPreserveRatio(true);
 
-		
 		myStatsList = new ListView<String>();
 		myStatsList.setOrientation(Orientation.HORIZONTAL);
 		stats = FXCollections.observableArrayList();
 		myStatsList.setItems(stats);
+		myStatsList.setMaxWidth(Double.MAX_VALUE);
+		HBox.setHgrow(myStatsList, Priority.ALWAYS);
+		myShopPane.bindHeight(myStatsList, myHBox.heightProperty());
 
 		myHBox.getChildren().addAll(myImageView, myStatsList);
 		myHBox.setAlignment(Pos.CENTER_LEFT);
@@ -60,17 +84,16 @@ public class CurrentView implements Observer {
 		IEntity entity = (IEntity) o;
 		Map<String, String> statMap = entity.getStats().getStatsMap();
 		for(String s: statMap.keySet()){
-			showMap.put(s, new Boolean(true));
+			if(!showMap.containsKey(s))
+				showMap.put(s, true);
 		}
-		showMap.keySet().retainAll(statMap.keySet());
-		
 		stats.clear();
 		for (String s : statMap.keySet()) {
 			if (s.equals("Image") && !statMap.get("Image").equals(myImageName)) {
 				myImageView.setImage(new Image(statMap.get("Image")));
 				myImageName = statMap.get("Image");
 			} else if (showMap.get(s)){
-				stats.add(s + ": " + statMap.get(s));
+				stats.add(myResources.getString(s) + ": " + statMap.get(s));
 			}
 		}
 	}
