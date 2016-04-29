fdf6a7e77015186d1f1ac3146a70f74248ee8a89
Frank Wang <fraankwang@gmail.com>
2016-04-29 02:44:18 -0400

    added extension of GridViewPanel for level-selecting in Mode

---
+++ src/authoring/frontend/display_elements/panels/LevelGridViewPanel.java
@@ -0,0 +1,19 @@
+package authoring.frontend.display_elements.panels;
+
+import java.util.*;
+
+import authoring.frontend.display_elements.tab_displays.TabDisplay;
+
+public class LevelGridViewPanel extends GridViewPanel {
+
+	public LevelGridViewPanel(int height, int width, TabDisplay tabDisplay) {
+		super(height, width, tabDisplay);
+	}
+	
+	public void populateLevels(TreeMap<String, String> map) {
+		for (String name : map.keySet()) {
+			
+		}
+	}
+
+}
