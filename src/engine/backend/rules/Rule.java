56b2895e936e428e35987a7f73282023fc894eb0
Kushal Byatnal <kbyatnal@gmail.com>
2016-04-30 18:32:34 -0400

    documentation for rules

--- src/engine/backend/rules/Rule.java
+++ src/engine/backend/rules/Rule.java
@@ -17,29 +17,53 @@ public class Rule {
 		myEvents = new ArrayList<String>();
 		myActions = new ArrayList<IAction>();
 	}
-
+	
+	/**
+	 * 
+	 * @param events - Collection of Strings representing eventIDs for this rule
+	 */
 	public void addEvents(Collection<String> events) {
 		for (String e : events) {
 			myEvents.add(e);
 		}
 	}
-
+	
+	/**
+	 * 
+	 * @param event - String eventID to add to this rule
+	 */
 	public void addEvents(String event) {
 		myEvents.add(event);
 	}
-
+	
+	/**
+	 * 
+	 * @param actions - Collection of IAction objects to add to this rule
+	 */
 	public void addActions(Collection<IAction> actions) {
 		actions.forEach(a -> myActions.add(a));
 	}
-
+	
+	/**
+	 * 
+	 * @param action IAction to add to this rule
+	 */
 	public void addActions(IAction action) {
 		myActions.add(action);
 	}
-
+	
+	/**
+	 * 
+	 * @return Collection of Strings representing this rule's event IDs (in the format entity_event)
+	 */
 	public Collection<String> getEvents() {
 		return myEvents;
 	}
-
+	
+	/**
+	 * 
+	 * @return Collection of IAction objects for this rule
+	 */
 	public Collection<IAction> getActions() {
 		return myActions;
 	}
