41e0f93cd14de3ca388d11b066c3e727cd696452
Christine Zhou <zhou.l.christine@gmail.com>
2016-04-30 20:36:59 -0400

    Modifying event manager.

--- src/engine/backend/systems/EventManager.java
+++ src/engine/backend/systems/EventManager.java
@@ -251,6 +251,7 @@ public class EventManager implements Observer {
 	private void resetLevel(){
 		String modeName = currentGameStatistics.getCurrentMode();
 		int levelIndex = currentGameStatistics.getCurrentLevelIndex();
+		System.out.println("reseting level here");
 		myGameWorld.getLevelWithId(modeName, levelIndex).setShouldRevert(true);
 	}
 
