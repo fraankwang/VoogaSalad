a180e07b1f15a21ccfb6f959a1a37d05893fe619
Ben Chesnut <benchesnut95@gmail.com>
2016-05-01 01:34:44 -0400

    pre test commit

--- src/authoring/frontend/editor_features/ImageImporter.java
+++ src/authoring/frontend/editor_features/ImageImporter.java
@@ -6,6 +6,8 @@ import java.io.IOException;
 import java.lang.reflect.Method;
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.imageio.ImageIO;
 import javafx.embed.swing.SwingFXUtils;
@@ -24,6 +26,7 @@ public class ImageImporter {
 	public static final String IMAGE_FOLDER = "src/resources/images/";
 	public static final String IMAGE_PATH = "resources/images/";
 	private ObjectChooser myChooser;
+	private Map<String, String> myImageMap;
 	
 	public ImageImporter(ObjectChooser chooser) {
 		myChooser = chooser;
@@ -31,9 +34,11 @@ public class ImageImporter {
 	
 	public void initialize() {
 		File imageFolder = new File(IMAGE_FOLDER);
+		myImageMap = new HashMap<String, String>();
 		for (final File fileEntry : imageFolder.listFiles()) {
 
 			myChooser.add(IMAGE_PATH + fileEntry.getName(), IMAGE_PATH + fileEntry.getName());
+			myImageMap.put(IMAGE_PATH + fileEntry.getName(), IMAGE_PATH + fileEntry.getName());
 		}
 	}
 	
@@ -48,32 +53,31 @@ public class ImageImporter {
 			e.printStackTrace();
 		}
 		
-		Class[] parameters = new Class[]{URL.class};
-		
-		URLClassLoader sysloader = (URLClassLoader) ClassLoader.getSystemClassLoader();
-		Class sysclass = URLClassLoader.class;
+//		Class[] parameters = new Class[]{URL.class};
+//		
+//		URLClassLoader sysloader = (URLClassLoader) ClassLoader.getSystemClassLoader();
+//		Class sysclass = URLClassLoader.class;
+//
+//		try {
+//			Method method = sysclass.getDeclaredMethod("addURL", parameters);
+//			method.setAccessible(true);
+//			method.invoke(sysloader, new Object[]{newFile.toURI().toURL()});
+//		} catch (Throwable t) {
+//		     t.printStackTrace();
+//		     throw new IOException("Error, could not add URL to system classloader");
+//		}
+//				
+//		for (URL url: sysloader.getURLs()) {
+//			System.out.println(url.toString());
+//		}
 
-		try {
-			Method method = sysclass.getDeclaredMethod("addURL", parameters);
-			method.setAccessible(true);
-			method.invoke(sysloader, new Object[]{newFile.toURI().toURL()});
-		} catch (Throwable t) {
-		     t.printStackTrace();
-		     throw new IOException("Error, could not add URL to system classloader");
-		}
-				
-		for (URL url: sysloader.getURLs()) {
-			System.out.println(url.toString());
-		}
-		String fileName = sysloader.getURLs()[sysloader.getURLs().length - 1].toString();
-
-		myChooser.add(fileName, fileName.split("src/")[1]);
+		myChooser.add(IMAGE_PATH + newFile.getName(), IMAGE_PATH + newFile.getName());
 	}
 	
 	public void openImporter() {
 		FileChooser fileChooser = new FileChooser();
 		fileChooser.setTitle("Open Resource File");
-		fileChooser.getExtensionFilters().addAll(new ExtensionFilter("Image Files", "*.png", "*.jpg", "*.gif"));
+		fileChooser.getExtensionFilters().addAll(new ExtensionFilter("Image Files", "*.png", "*.jpg", "*.jpeg", "*.gif"));
 		File imageFile = fileChooser.showOpenDialog(null);
 		if (imageFile != null) {
 			try {
