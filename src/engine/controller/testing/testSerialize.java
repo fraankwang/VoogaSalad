5bcd51d65d5b44bacaed48ec661242083b7a7b6f
Austin Wu <austintwu@gmail.com>
2016-05-02 13:47:15 -0400

    hi

--- src/engine/controller/testing/testSerialize.java
+++ src/engine/controller/testing/testSerialize.java
@@ -2,17 +2,16 @@ package engine.controller.testing;
 
 import java.io.IOException;
 
-import authoring.backend.Testing;
 import backend.xml_converting.GameWorldToXMLWriter;
 import engine.backend.game_object.GameWorld;
 
 public class testSerialize {
 
 	public testSerialize() throws IOException {
-		Galaga g = new Galaga();
+		ITestingGame g = new Galaga();
 		GameWorld game = g.initGame();
 		GameWorldToXMLWriter serializer = new GameWorldToXMLWriter();
-		String xml = serializer.getXMLfromObject(g);
+		String xml = serializer.getXMLfromObject(game);
 
 		GameWorldToXMLWriter.stringToDocument(xml, "/Users/austinwu/Desktop/galagaGame.xml");
 	}
