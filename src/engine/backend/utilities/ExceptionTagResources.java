0dfe3d648acce6f0cb40030afcc18a375c028db4
Kushal Byatnal <kbyatnal@gmail.com>
2016-05-07 21:08:34 -0400

    Refactor

---
+++ src/engine/backend/utilities/ExceptionTagResources.java
@@ -0,0 +1,19 @@
+// This entire file is part of my masterpiece.
+// Kushal Byatnal
+
+package engine.backend.utilities;
+
+public class ExceptionTagResources {
+	
+	public static final String LACK_ACCESS = "LackAccessToClass";
+	public static final String METHOD_DNE = "MethodDoesNotExist";
+	public static final String SECURITY_EXCEPTION = "SecurityException";
+	public static final String ILLEGAL_ARGS = "IllegalArguments";
+	public static final String INSTANTIATION = "ReflectionInstantiation";
+	public static final String ENTITY_DNE = "EntityDNEInMap";
+	
+	public ExceptionTagResources() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
